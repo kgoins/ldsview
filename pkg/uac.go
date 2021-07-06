4edd988b91d1dd5752a632e46844dcf04487d4c5
kgoins <14044530+kgoins@users.noreply.github.com>
2021-07-06 18:22:49 -0400

    removed UACSearch from the lib since a search for objects by UAC value can be performed by the search or value APIs

--- pkg/uac.go
+++ pkg/uac.go
@@ -8,55 +8,11 @@ import (
 	"text/tabwriter"
 
 	"github.com/audibleblink/msldapuac"
-	"github.com/kgoins/ldapentity/entity"
 )
-
-type UACFilter struct {
-	searchProp int
-}
-
-func NewUACFilter(searchTerm int) UACFilter {
-	return UACFilter{searchProp: searchTerm}
-}
-
-func (filter UACFilter) Matches(entity entity.Entity) (isMatch bool) {
-	uacAttr, found := entity.GetAttribute("userAccountControl")
-	if found {
-		uacString := uacAttr.Value.GetSingleValue()
-		uac, _ := strconv.ParseInt(uacString, 10, 32)
-		isMatch, err := msldapuac.IsSet(uac, filter.searchProp)
-		if err != nil {
-			fmt.Printf("Unable to verify UAC: %v\n", err.Error())
-			return false
-		}
-		return isMatch
-	}
-	return
-}
 
 // GetFlagsFromUAC wraps the msldapuac dependency for easy re-definition
 func GetFlagsFromUAC(uac int64) ([]string, error) {
 	return msldapuac.ParseUAC(uac)
-}
-
-// UACSearch will seek for entities who have the given UAC property set
-func UACSearch(entities *[]entity.Entity, uacProp int) (matches []entity.Entity) {
-	for _, entity := range *entities {
-		uacStr, found := entity.GetSingleValuedAttribute("userAccountControl")
-		if !found {
-			continue
-		}
-
-		i64, _ := strconv.ParseInt(uacStr, 10, 32)
-		isMatch, err := msldapuac.IsSet(i64, uacProp)
-		if err != nil {
-			continue
-		}
-		if isMatch {
-			matches = append(matches, entity)
-		}
-	}
-	return
 }
 
 // UACPrint prints the available UAC options that are available for searching
@@ -79,7 +35,7 @@ func UACPrint(dest io.Writer) {
 	}
 }
 
-//UACParse take a UAC int and return
+// UACParse take a UAC int and return the names of the flags it has set
 func UACParse(uacValue string) (flagNames []string, err error) {
 	uacInt, err := strconv.ParseInt(uacValue, 10, 64)
 	if err != nil {
