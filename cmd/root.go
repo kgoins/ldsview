0d6fd72b840a2206533ee7a2e837ddd2843acade
kgoins <14044530+kgoins@users.noreply.github.com>
2021-10-10 21:37:45 -0400

    rm'd initConfig in root cmd in favor of snakecharmer

--- cmd/root.go
+++ cmd/root.go
@@ -7,12 +7,7 @@ import (
 	"github.com/kgoins/ldsview/internal"
 	"github.com/kgoins/snakecharmer/snakecharmer"
 	"github.com/spf13/cobra"
-
-	homedir "github.com/mitchellh/go-homedir"
-	"github.com/spf13/viper"
 )
-
-var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -43,10 +38,7 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-
-	rootCmd.PersistentFlags().StringVar(
-		&cfgFile,
+	rootCmd.PersistentFlags().String(
 		"config",
 		"",
 		"config file (default is $HOME/.ldsview.yaml)",
@@ -78,30 +70,4 @@ func init() {
 		false,
 		"Print version information",
 	)
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".ldsview" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".ldsview")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
 }
